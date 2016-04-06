92ddc86f94a54e71285feb6af3c48a27550a658b
stevula <brodericksteven@gmail.com>
2016-04-06 15:33:32 -0700

    Finish multiply method

--- arithmetic.rb
+++ arithmetic.rb
@@ -2,6 +2,7 @@ def add(num_1, num_2)
   num_2.times do
     num_1 = num_1.next
   end
+
   num_1
 end
 
@@ -9,6 +10,12 @@ def subtract(num_1, num_2)
 end
 
 def multiply(num_1, num_2)
+  sum = 0
+  num_2.times do 
+    sum = add(sum, num_1)
+  end
+
+  sum
 end
 
 def divide(num_1, num_2)
