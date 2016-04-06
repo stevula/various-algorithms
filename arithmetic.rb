afef01c892d27c569f9fd19589816703ab854a6b
stevula <brodericksteven@gmail.com>
2016-04-06 15:30:21 -0700

    Create add method

---
+++ arithmetic.rb
@@ -0,0 +1,16 @@
+def add(num_1, num_2)
+  num_2.times do
+    num_1 = num_1.next
+  end
+  num_1
+end
+
+def subtract(num_1, num_2)
+end
+
+def multiply(num_1, num_2)
+end
+
+def divide(num_1, num_2)
+end
+
