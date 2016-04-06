a083bfb49fca2a9b7a5458e4de27343370e753fd
stevula <brodericksteven@gmail.com>
2016-04-06 14:24:46 -0700

    Incorporate #round function and refactor

--- pi_to_the_nth_digit.rb
+++ pi_to_the_nth_digit.rb
@@ -12,32 +12,23 @@ require 'bigdecimal/math'
 #   end
 
 def valid_integer?(string)
-  string.to_i.to_s == string && string.to_i > 0
+  string.to_i.to_s == string
 end
 
-puts "To how many digits would you like to get pi?"
-digits = gets.chomp
+puts "To how many decimal places would you like to get pi?"
+decimal_places = gets.chomp
 
 # confirm that value is an integer string
-until valid_integer?(digits)
+until valid_integer?(decimal_places)
   puts "Enter a valid integer."
 
-  digits = gets.chomp
+  decimal_places = gets.chomp
 end
 
-digits = digits.to_i
+decimal_places = decimal_places.to_i
 
-decimal_pi = BigMath.PI(digits).to_s
-
-
-pi = decimal_pi[2]
-# only add a decimal to the string if requested digits > 1
-pi += "." + decimal_pi[3..digits + 1] if digits > 1
-
-# handle rounding of last digit
-if decimal_pi[digits + 2] >= "5"
-  # add 1
-  pi[-1] = (pi[-1].to_i + 1).to_s
-end
+decimal_pi = BigMath.PI(decimal_places + 1).round(decimal_places).to_s
+pi = "3"
+pi += "." + decimal_pi[3..-3] if decimal_places >= 1
 
 puts "pi = #{pi}"
